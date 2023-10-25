7024e315714e01de7956d467b264f7c6efe5c8c1
Gabriela Sanches <gabi@toki.life>
2023-10-25 14:45:37 -0300

    config to deploy

--- show-case/astro.config.mjs
+++ show-case/astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gabrielarsanches.github.io',
-  base: '/show-case-gabriela-sanches',
+  base: '/gabrielarsanches.github.io',
 });
