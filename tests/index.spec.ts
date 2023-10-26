97e3e15557e5231153b1fe18f80fbef8a11beec3
Gabriela Sanches <gabi@toki.life>
2023-10-26 19:09:45 -0300

    refactor description

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -26,7 +26,7 @@ test.describe('ui test', () => {
 		const metaDescription = page.locator("meta[name='description']");
 		await expect(metaDescription).toHaveAttribute(
 			'content',
-			'A heavily optimized description full of well-researched keywords.'
+			'I am Gabriela and this is my portifolio'
 		);
 		const html = page.locator('html');
 		await expect(html).toHaveClass('scroll-smooth');
